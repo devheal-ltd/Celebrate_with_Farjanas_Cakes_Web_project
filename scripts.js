d524a4a9154731db7cdda87f630bf3e95e505a9a
devheal-ltd <robiulislam4020@gmail.com>
2024-07-29 09:34:28 -0700

    product heading, description,add

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // WhatsApp যোগাযোগ ফাংশন
 function contactWhatsApp(product) {
-    const phoneNumber = '880123456789'; // আপনার ফোন নম্বর
+    const phoneNumber = '+8801734999464'; // আপনার ফোন নম্বর
     const message = `Hi, I'm interested in ${product}.`;
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
@@ -8,7 +8,7 @@ function contactWhatsApp(product) {
 
 // Messenger যোগাযোগ ফাংশন
 function contactMessenger(product) {
-    const pageLink = 'https://m.me/আপনারপেজনাম'; // পেজ নাম এখানে লিখুন
+    const pageLink = 'https://m.me/CelebratewithFarjanasCakes'; // পেজ নাম এখানে
     const message = `Hi, I'm interested in ${product}.`;
     const url = `${pageLink}?message=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
@@ -16,6 +16,6 @@ function contactMessenger(product) {
 
 // Call যোগাযোগ ফাংশন
 function contactCall() {
-    const phoneNumber = 'tel:+880123456789'; // আপনার ফোন নম্বর
+    const phoneNumber = 'tel:+8801734999464'; // আপনার ফোন নম্বর
     window.open(phoneNumber, '_self');
 }
